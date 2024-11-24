52b486d18032bf52f6aa97e6a2bc820a7ea03494
gabimatatov <gavriel.matatov@veeva.com>
2024-11-24 17:50:12 +0200

    craeted controller for posts

---
+++ controllers/posts.js
@@ -0,0 +1,34 @@
+const Post = require('../models/posts');
+
+// Get all posts
+const getAllPosts = async (req, res) => {
+
+};
+
+// Create a new post
+const createNewPost = async (req, res) => {
+
+};
+
+// Delete a post
+const deletePost = async (req, res) => {
+
+};
+
+// Get post by id
+const getPostByid = async (req, res) => {
+
+};
+
+// Update a post
+const updatePost = async (req, res) => {
+
+};
+
+module.exports = {
+    getAllPosts,
+    createNewPost,
+    deletePost,
+    getPostByid,
+    updatePost
+};
