fc00e824573b9115520f6a8f7311bde598edf03d
gternovs <gternovs@akamai.com>
2024-12-21 15:06:54 +0200

    Disabled no-var check of lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { 
+    files: ["**/*.{js,mjs,cjs,ts}"], 
+    rules: {
+      "no-var": "error", // Enforce no-var rule
+    }
+  },
+  { 
+    languageOptions: { globals: globals.browser } 
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
