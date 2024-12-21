c6b473c8bac3fc572a2f19ef94198edb1f190c01
gabimatatov <gavriel.matatov@veeva.com>
2024-12-22 00:30:21 +0200

    created auth controller

---
+++ src/controllers/auth_controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+
+const register = async (req: Request, res: Response) => {
+    return;
+}
+
+const login = async (req: Request, res: Response) => {
+    return;
+}
+
+export default {
+    register, 
+    login
+}
